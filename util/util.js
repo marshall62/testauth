3528eacb9ecf015b6e3b6504dc8ae29a8534a0a0
david <marshall62@gmail.com>
2016-11-15 09:55:40 -0500

    fixed bug in util.

--- util/util.js
+++ util/util.js
@@ -1,10 +1,12 @@
 
 function getFullURL (req) {
     var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+    return fullUrl;
 }
 
 function getPageContext (req) {
     var fullUrl = req.protocol + '://' + req.get('host') + global.context;
+    return fullUrl;
 }
 
 var Util = module.exports = {
