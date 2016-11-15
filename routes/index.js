b3b084c7fbe7bcb6aedc890db3a8a73a860da1fa
david <marshall62@gmail.com>
2016-11-15 10:37:57 -0500

    changed index.js to redirect to login

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var fs = require("fs");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.redirect('login');
 });
 
 router.post('/file_upload', function (req, res) {
