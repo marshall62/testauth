e7a8f7d9b97f354350d2688d4edcbe60d37f806d
david <marshall62@gmail.com>
2017-01-11 15:03:09 -0500

    why is this pushing when its in the gitignore

--- util/config.js
+++ util/config.js
@@ -17,6 +17,6 @@ config.db.host="localhost";
 // correct setting for rose server
 // config.pageContext = "testauth";
 // correct setting for development environment
-config.pageContext = "";
+config.pageContext = "testauthdd";
 
 module.exports = config;
